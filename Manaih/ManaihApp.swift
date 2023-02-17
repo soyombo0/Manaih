15842a36922780d656d2d72b8e406224b0079946
soyombo <soyombo0@naver.com>
2023-02-17 22:34:29 +0300

    Initial Commit

---
+++ Manaih/ManaihApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManaihApp.swift
+//  Manaih
+//
+//  Created by Soyombo Mantaagiin on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ManaihApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
