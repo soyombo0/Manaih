d3e47ebd6a8c5f0401b04b0f0edfde2708889e9f
soyombo <soyombo0@naver.com>
2023-02-20 23:21:23 +0300

    com

--- Manaih/View/Home.swift
+++ Manaih/View/Home.swift
@@ -16,7 +16,25 @@ struct Home: View {
     
     var body: some View {
         if let info = quizInfo {
-            Text(info.title)
+            VStack(spacing: 10) {
+                Text(info.title)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .hAllign(.leading)
+                
+                CustomLabel("list.bullet.rectangle.portrait", "\(questions.count)", "Multiple Choice Questions")
+                    .padding(.top, 20)
+                
+                CustomLabel("person", "\(info.peopleAttended)", "Attended the exercise")
+                    .padding(.top, 5)
+                
+                Divider()
+                    .padding(.horizontal, -15)
+                    .padding(.top,15)
+            }
+            .padding(15)
+            
+            
         } else {
             VStack {
                 ProgressView()
@@ -33,6 +51,35 @@ struct Home: View {
                 
             }
         }
+    }
+    
+    @ViewBuilder
+    func CustomLabel(_ image: String, _ title: String, _ subTitle: String) -> some View {
+        HStack(spacing: 12) {
+            Image(systemName: image)
+                .font(.title3)
+                .frame(width: 45, height: 45)
+                .background {
+                    Circle()
+                        .fill(.gray.opacity(0.1))
+                        .padding(-1)
+                        .background {
+                            Circle()
+                                .stroke(Color("BG"), lineWidth: 1)
+                        }
+                }
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("BG"))
+                Text(subTitle)
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .foregroundColor(.gray)
+            }
+            .hAllign(.leading)
+        }
+        
     }
     
     /// - Fetching
@@ -60,5 +107,18 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+
+extension View {
+    func hAllign(_ alignment: Alignment) -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    func vAllign(_ alignment: Alignment) -> some View {
+        self
+            .frame(maxHeight: .infinity, alignment: alignment)
     }
 }
